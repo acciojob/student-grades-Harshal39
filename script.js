1b080b621b78e21f8c20da19fad9b9f28305381f
Harshal Bhute <harshalbhute39@gmail.com>
2025-02-04 20:32:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
 let data = fetch('students.json').
-	then(response => response.json).
+	then(response => response.json()).
 	then(console.log(data)).
 	catch(err => console.log(err));
